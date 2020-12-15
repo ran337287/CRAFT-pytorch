cc1dcc0f793aa6f4d69991793ce469a17e4a0f41
ran337287 <belongtoabcd@126.com>
2020-12-15 12:41:59 +0800

    Update imgproc.py

--- imgproc.py
+++ imgproc.py
@@ -8,11 +8,11 @@ import numpy as np
 from skimage import io
 import cv2
 
-def loadImage(img_file):
-    img = io.imread(img_file)           # RGB order
-    if img.shape[0] == 2: img = img[0]
-    if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    if img.shape[2] == 4:   img = img[:,:,:3]
+def loadImage(img_file): #读取RGB
+    img = io.imread(img_file)   # W*H*C       # RGB order
+    if img.shape[0] == 2: img = img[0]  #若
+    if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB) # len(img.shape)==2 为灰度图
+    if img.shape[2] == 4:   img = img[:,:,:3] #RGBA图像时，选取RGB通道
     img = np.array(img)
 
     return img
